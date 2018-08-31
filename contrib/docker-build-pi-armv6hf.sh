64f6644224859cb1e8a8c09cc071c001724b1d3a
ashthespy <ashthespy@gmail.com>
2018-08-31 19:56:40 +0200

    Tweak cross compilation environment

--- contrib/docker-build-pi-armv6hf.sh
+++ contrib/docker-build-pi-armv6hf.sh
@@ -16,14 +16,16 @@ SYSROOT="/pi-tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/arm-bcm2708hardfp
 GCC="/pi-tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin"
 GCC_SYSROOT="$GCC/gcc-sysroot"
 
-
 export PATH=/pi-tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/:$PATH
 
 # Link the compiler
 export TARGET_CC="$GCC/arm-linux-gnueabihf-gcc"
 
 # Create wrapper around gcc to point to rpi sysroot
-echo -e '#!/bin/bash' "\n$TARGET_CC --sysroot $SYSROOT \"\$@\"" > $GCC_SYSROOT
+echo -e '#!/bin/bash' "\n$TARGET_CC --sysroot $SYSROOT \
+      -L${SYSROOT}/usr/lib/arm-linux-gnueabihf \
+      -L${SYSROOT}/usr/lib \
+      \"\$@\"" > $GCC_SYSROOT
 chmod +x $GCC_SYSROOT
 
 # Add extra target dependencies to our rpi sysroot
@@ -39,4 +41,5 @@ done
 echo -e '[target.arm-unknown-linux-gnueabihf]\nlinker = "gcc-sysroot"' > /.cargo/config
 
 # Build
+export PKG_CONFIG_ALLOW_CROSS=1
 cargo build --release --target arm-unknown-linux-gnueabihf --no-default-features --features "alsa-backend"
